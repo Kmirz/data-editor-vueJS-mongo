3854707d18a23dce0d989e4a969ba7e51c5e3594
K Mirz <104242241+Kmirz@users.noreply.github.com>
2022-05-27 00:24:29 -0400

    commenting out static code

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const subscribersRouter = require("./app/Routes/subscribers");
 
 const app = express();
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
-const db = mongoose.connection;
-db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("Connected to Database"));
+// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+// const db = mongoose.connection;
+// db.on("error", (error) => console.error(error));
+// db.once("open", () => console.log("Connected to Database"));
 
 app.use(express.json());
 app.use(express.static("dist"));
